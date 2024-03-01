1e9615ad997f920702d1a288da420065fdc2d8ba
Bart Bilos <squantmuts+git@gmail.com>
2024-03-01 14:24:02 +0100

    updated and cut down files to have a barebones implementation

--- firmware/common/src/nuclone_LPC845M301BD48_tests.cpp
+++ firmware/common/src/nuclone_LPC845M301BD48_tests.cpp
@@ -1,13 +1,14 @@
 /*
  * SPDX-License-Identifier: MIT
  *
- * Copyright (c) 2021 Bart Bilos
+ * Copyright (c) 2024 Bart Bilos
  * For conditions of distribution and use, see LICENSE file
  */
 #include <nuclone_LPC845M301BD48_tests.hpp>
 
 void crudeDelay(uint32_t iterations) {
   for (uint32_t i = iterations; i > 0; i--) {
+    libMcuLL::sw::nop();
   }
 }
 
