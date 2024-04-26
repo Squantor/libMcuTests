a21486873e04ab5746cf208184bf260884993122
Bart Bilos <squantmuts+git@gmail.com>
2024-04-26 10:17:11 +0200

    added gpio pin muxing tests

--- firmware/projects/LL_RP2040/src/test_RP2040_gpio.cpp
+++ firmware/projects/LL_RP2040/src/test_RP2040_gpio.cpp
@@ -12,21 +12,39 @@
 #include <RP2040_teardown.hpp>
 #include <common.hpp>
 
-using namespace libMcuLL::hw::sio;
-// using namespace libMcuLL::sw::sio;
+using namespace libMcuLL;
 
 // peripheral register sets
-static constexpr libMcuLL::hwAddressType dutAddress = libMcuLL::hw::sioAddress;
-libMcuLL::hw::sio::peripheral *const dutRegisters{reinterpret_cast<libMcuLL::hw::sio::peripheral *>(dutAddress)};
+static constexpr hwAddressType dutAddress = hw::gpioBank0Address;
+hw::gpioBank0::peripheral *const dutRegisters{reinterpret_cast<hw::gpioBank0::peripheral *>(dutAddress)};
 
 /**
  * @brief sio setup and initialisation
  */
 MINUNIT_SETUP(RP2040SetupSio) {
-  // minUnitCheck(LPC812M101TeardownCorrect() == true);
-  minUnitPass();
+  minUnitCheck(RP2040TeardownCorrect() == true);
+  resetsPeripheral.reset(sw::resets::IO_BANK0 | sw::resets::PADS_BANK0, 100000);
+  // connect all GPIO's
+  gpioBank0Peripheral.setup(gpio0Pin);
+  gpioBank0Peripheral.setup(gpio1Pin);
+  gpioBank0Peripheral.setup(gpio2Pin);
+  gpioBank0Peripheral.setup(gpio3Pin);
+  gpioBank0Peripheral.setup(gpio4Pin);
+  gpioBank0Peripheral.setup(gpio5Pin);
 }
 
-MINUNIT_ADD(RP2040gpio, RP2040SetupSio, NULL) {
+MINUNIT_ADD(RP2040gpio, RP2040SetupSio, RP2040Teardown) {
+  minUnitCheck(dutRegisters->GPIO[0].STATUS == 0x0000'0000u);
+  minUnitCheck(dutRegisters->GPIO[0].CTRL == 0x0000'0005u);
+  minUnitCheck(dutRegisters->GPIO[1].STATUS == 0x0000'0000u);
+  minUnitCheck(dutRegisters->GPIO[1].CTRL == 0x0000'0005u);
+  minUnitCheck(dutRegisters->GPIO[2].STATUS == 0x0000'0000u);
+  minUnitCheck(dutRegisters->GPIO[2].CTRL == 0x0000'0005u);
+  minUnitCheck(dutRegisters->GPIO[3].STATUS == 0x0000'0000u);
+  minUnitCheck(dutRegisters->GPIO[3].CTRL == 0x0000'0005u);
+  minUnitCheck(dutRegisters->GPIO[4].STATUS == 0x0000'0000u);
+  minUnitCheck(dutRegisters->GPIO[4].CTRL == 0x0000'0005u);
+  minUnitCheck(dutRegisters->GPIO[5].STATUS == 0x0000'0000u);
+  minUnitCheck(dutRegisters->GPIO[5].CTRL == 0x0000'0005u);
   minUnitPass();
 }
