b0b57d324d3a09ca36d1528fe56cace103dcb0e4
Bart Bilos <squantmuts+git@gmail.com>
2024-06-06 10:44:09 +0200

    working uart read/write

--- firmware/projects/LL_RP2040/src/test_RP2040_LL_uart.cpp
+++ firmware/projects/LL_RP2040/src/test_RP2040_LL_uart.cpp
@@ -14,6 +14,7 @@
 
 using namespace libMcuLL;
 using namespace libMcuLL::sw::uart;
+using namespace libMcuLL::hw::uart;
 
 // peripheral register sets
 static constexpr hwAddressType uart0Address = hw::uart0Address;
@@ -53,4 +54,32 @@ MINUNIT_ADD(RP2040LLUartSetup, RP2040LLSetupUart, RP2040Teardown) {
   // check control registers and divisors and such
   minUnitCheck(uartPeripheral.setup(1000000) == 1000000);
   // check control registers and divisors and such
+}
+
+MINUNIT_ADD(RP2040LLUartComms, RP2040LLSetupUart, RP2040Teardown) {
+  std::array<std::uint8_t, 1> singleData{0x5A};
+  std::array<std::uint8_t, 5> multiData{0x01, 0x23, 0x45, 0x67, 0xA5};
+  std::array<std::uint8_t, 5> receiveDataMulti;
+  std::array<std::uint8_t, 1> receiveDataSingle;
+  minUnitCheck(uartPeripheral.setup(9600) == 9600);
+  minUnitCheck(uart0Registers->UARTFR & UARTFR::TXFE_FLAG);
+  minUnitCheck(uart0Registers->UARTFR & UARTFR::RXFE_FLAG);
+  minUnitCheck(uartPeripheral.read(receiveDataSingle, 100) == libMcu::results::TIMEOUT);
+  uartPeripheral.write(singleData);
+  minUnitCheck(uart0Registers->UARTFR & UARTFR::TXFE_FLAG);
+  libMcu::sw::delay(1000000);                                   // wait until data has been received
+  minUnitCheck(!(uart0Registers->UARTFR & UARTFR::RXFE_FLAG));  // we looped TX and RX so RX FIFO should also not be empty
+  minUnitCheck(uartPeripheral.read(receiveDataSingle, 100) == libMcu::results::NO_ERROR);
+  minUnitCheck(receiveDataSingle[0] == 0x5A);
+  minUnitCheck(uart0Registers->UARTFR & UARTFR::TXFE_FLAG);
+  minUnitCheck(uart0Registers->UARTFR & UARTFR::RXFE_FLAG);
+  uartPeripheral.write(multiData);
+  minUnitCheck(uartPeripheral.read(receiveDataMulti, 10000) == libMcu::results::NO_ERROR);
+  minUnitCheck(uart0Registers->UARTFR & UARTFR::TXFE_FLAG);
+  minUnitCheck(uart0Registers->UARTFR & UARTFR::RXFE_FLAG);
+  minUnitCheck(receiveDataMulti[0] == 0x01);
+  minUnitCheck(receiveDataMulti[1] == 0x23);
+  minUnitCheck(receiveDataMulti[2] == 0x45);
+  minUnitCheck(receiveDataMulti[3] == 0x67);
+  minUnitCheck(receiveDataMulti[4] == 0xA5);
 }
