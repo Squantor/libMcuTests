9adcadb0e4818300429d0359e10d74f6f53690d0
Bart Bilos <squantmuts+git@gmail.com>
2024-08-02 10:10:14 +0200

    added pin definitions

--- firmware/common/inc/nuclone_LPC845BD48_small_LL.hpp
+++ firmware/common/inc/nuclone_LPC845BD48_small_LL.hpp
@@ -25,24 +25,21 @@
 using xtalInPinType = libMcu::hw::pin<libMcu::hw::IOports::PORT0, libMcu::hw::IOpins::PIN08>;
 using xtalOutPinType = libMcu::hw::pin<libMcu::hw::IOports::PORT0, libMcu::hw::IOpins::PIN09>;
 using testPinType = libMcu::hw::pin<libMcu::hw::IOports::PORT1, libMcu::hw::IOpins::PIN05>;
-// connected pin 0 and 1 via 3.3K resistors, links UART RX/TX, PIN1 on board
-// using gpio0PinType = libMcu::hw::pin<libMcu::hw::IOports::PORT0, libMcu::hw::IOpins::PIN00, libMcu::hw::IOfuncts::SIO>;
-// using gpio1PinType = libMcu::hw::pin<libMcu::hw::IOports::PORT0, libMcu::hw::IOpins::PIN01, libMcu::hw::IOfuncts::SIO>;
-// using uartOutPinType = libMcu::hw::pin<libMcu::hw::IOports::PORT0, libMcu::hw::IOpins::PIN00, libMcu::hw::IOfuncts::UART0_TX>;
-// using uartInPinType = libMcu::hw::pin<libMcu::hw::IOports::PORT0, libMcu::hw::IOpins::PIN01, libMcu::hw::IOfuncts::UART0_RX>;
+// connected pin 1 and 2 via 3.3K resistors, links UART RX/TX, PIN1 on board
+using pin1Type = libMcu::hw::pin<libMcu::hw::IOports::PORT1, libMcu::hw::IOpins::PIN08>;
+using pin2Type = libMcu::hw::pin<libMcu::hw::IOports::PORT0, libMcu::hw::IOpins::PIN17>;
 // connected pin 3 and 4 via 3.3K resistors, links SPI MOSI/MISO, PIN3 on board
-// using gpio2PinType = libMcu::hw::pin<libMcu::hw::IOports::PORT0, libMcu::hw::IOpins::PIN03, libMcu::hw::IOfuncts::SIO>;
-// using gpio3PinType = libMcu::hw::pin<libMcu::hw::IOports::PORT0, libMcu::hw::IOpins::PIN04, libMcu::hw::IOfuncts::SIO>;
-// using spiOutPinType = libMcu::hw::pin<libMcu::hw::IOports::PORT0, libMcu::hw::IOpins::PIN03, libMcu::hw::IOfuncts::SPI0_TX>;
-// using spiInPinType = libMcu::hw::pin<libMcu::hw::IOports::PORT0, libMcu::hw::IOpins::PIN04, libMcu::hw::IOfuncts::SPI0_RX>;
-// connected pin 2 and 5 via 3.3K resistors, links SPI CS/SCK, PIN2 on board
-// using gpio4PinType = libMcu::hw::pin<libMcu::hw::IOports::PORT0, libMcu::hw::IOpins::PIN02, libMcu::hw::IOfuncts::SIO>;
-// using gpio5PinType = libMcu::hw::pin<libMcu::hw::IOports::PORT0, libMcu::hw::IOpins::PIN05, libMcu::hw::IOfuncts::SIO>;
-// using spiSckPinType = libMcu::hw::pin<libMcu::hw::IOports::PORT0, libMcu::hw::IOpins::PIN02, libMcu::hw::IOfuncts::SPI0_SCK>;
-// using spiCsPinType = libMcu::hw::pin<libMcu::hw::IOports::PORT0, libMcu::hw::IOpins::PIN05, libMcu::hw::IOfuncts::SPI0_CS>;
-// connected pin 16 to 2.2K pull up resistor, SDA
+using pin3Type = libMcu::hw::pin<libMcu::hw::IOports::PORT0, libMcu::hw::IOpins::PIN13>;
+using pin4Type = libMcu::hw::pin<libMcu::hw::IOports::PORT0, libMcu::hw::IOpins::PIN18>;
+// connected pin 5 and 6 via 3.3K resistors, links SPI CS/SCK, PIN2 on board
+using pin5Type = libMcu::hw::pin<libMcu::hw::IOports::PORT1, libMcu::hw::IOpins::PIN09>;
+using pin6Type = libMcu::hw::pin<libMcu::hw::IOports::PORT0, libMcu::hw::IOpins::PIN19>;
+// connected pin 7 and 8 via 3.3K resistors
+using pin7Type = libMcu::hw::pin<libMcu::hw::IOports::PORT0, libMcu::hw::IOpins::PIN04>;
+using pin8Type = libMcu::hw::pin<libMcu::hw::IOports::PORT0, libMcu::hw::IOpins::PIN20>;
+// SDA pin with 2.2K pull up resistor
 using i2cSdaPinType = libMcu::hw::pin<libMcu::hw::IOports::PORT0, libMcu::hw::IOpins::PIN11>;
-// connected pin 17 to 2.2K pull up resistor, SCL
+// SCL pin with 2.2K pull up resistor
 using i2cSclPinType = libMcu::hw::pin<libMcu::hw::IOports::PORT0, libMcu::hw::IOpins::PIN10>;
 
 // function types
@@ -58,6 +55,14 @@ constexpr xtalOutPinType xtalOutPin;
 constexpr testPinType testPin;
 constexpr i2cSclPinType i2cSclPin;
 constexpr i2cSdaPinType i2cSdaPin;
+constexpr pin1Type testPin1;
+constexpr pin2Type testPin2;
+constexpr pin3Type testPin3;
+constexpr pin4Type testPin4;
+constexpr pin5Type testPin5;
+constexpr pin6Type testPin6;
+constexpr pin7Type testPin7;
+constexpr pin8Type testPin8;
 
 // function instances
 constexpr xtalInFunctionType xtalInFunction;
