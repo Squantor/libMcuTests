0d43cfcee962c2b4aa787dd29cff2cd0f84f4577
Bart Bilos <squantmuts+git@gmail.com>
2024-11-08 11:11:47 +0100

    WIP gpio STM32F031

--- firmware/common/inc/nuclone_STM32F031K6T_LL.hpp
+++ firmware/common/inc/nuclone_STM32F031K6T_LL.hpp
@@ -7,12 +7,6 @@
 #ifndef BOARD_TESTS_HPP
 #define BOARD_TESTS_HPP
 
-#define CLOCK_XTAL (12000000u)
-#define CLOCK_EXT_IN (0u)
-#define CLOCK_CPU (12000000u)
-#define CLOCK_AHB (12000000u)
-#define CLOCK_MAIN (12000000u)
-
 #include <st/libmcu_STM32F031K6T_ll.hpp>
 
 void boardInit(void);
