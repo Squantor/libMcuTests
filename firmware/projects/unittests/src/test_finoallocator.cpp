6a0a51cf4ea0ae5442005ae0193315201e1b5f00
Bart Bilos <squantmuts+git@gmail.com>
2026-03-20 10:32:28 +0100

    updated tests for FiNo allocator

--- firmware/projects/unittests/src/test_finoallocator.cpp
+++ firmware/projects/unittests/src/test_finoallocator.cpp
@@ -19,7 +19,7 @@
 #include <libmcu/assertions.hpp>
 #include <libmcu/finoallocator.hpp>
 
-libmcu::FinoAllocator<std::uint8_t, 10> fino_allocator_dut_u8;
+libmcu::Fino_allocator<std::uint8_t, 10> fino_allocator_dut_u8;
 
 MINUNIT_SETUP(FinoAllocatorSetup) {
   MINUNIT_PASS();
@@ -30,15 +30,15 @@ MINUNIT_TEARDOWN(FinoAllocatorTeardown) {
 }
 
 MINUNIT_ADD(FinoAllocatorRequest, FinoAllocatorSetup, FinoAllocatorTeardown) {
-  std::span<std::uint8_t> block_one = fino_allocator_dut_u8.Request(5);
+  std::span<std::uint8_t> block_one = fino_allocator_dut_u8.request(5);
   MINUNIT_CHECK(block_one.size() == 5);
   FillSpan(block_one, block_one.size());
   MINUNIT_CHECK(block_one[0] == block_one.size());
-  std::span<std::uint8_t> block_two = fino_allocator_dut_u8.Request(4);
+  std::span<std::uint8_t> block_two = fino_allocator_dut_u8.request(4);
   MINUNIT_CHECK(block_two.size() == 4);
   FillSpan(block_two, block_two.size());
   MINUNIT_CHECK(block_two[0] == block_two.size());
-  std::span<std::uint8_t> block_three = fino_allocator_dut_u8.Request(4);
+  std::span<std::uint8_t> block_three = fino_allocator_dut_u8.request(4);
   MINUNIT_CHECK(block_three.size() == 4);
   FillSpan(block_three, block_three.size());
   MINUNIT_CHECK(block_three[0] == block_three.size());
